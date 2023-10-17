55a23b28a1a9562068e9dd844323515b6f07c848
yvan-frank <yvanzangue@gmail.com>
2023-10-17 11:12:40 +0100

    dependencies added

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const HomeScreen = () => {
+	return (
+		<View className='flex-1 justify-center items-center'>
+			<Text>Home Screen</Text>
+		</View>
+	);
+};
+
+export default HomeScreen;
